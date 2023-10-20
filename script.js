b268450291d9c779098f95ad88a1f99141d19452
Ruslan Lopushan <tguruslan1994@gmail.com>
2023-10-20 12:59:18 +0300

    додано генерування по масці

--- script.js
+++ script.js
@@ -1,44 +1,77 @@
-function genPassword(list1=[], list2=[], type="simple"){
-    word1=list1[Math.floor(Math.random() * list1.length)].trim().toLowerCase()
-    word2=list2[Math.floor(Math.random() * list2.length)].trim().toLowerCase()
-    const words = `${word1}${word2.charAt(0).toUpperCase() + word2.slice(1)}`;
-    const numbers = `${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}`;
-  
-    const replaces = {
-        'a': '@',
-        'c': '(',
-        'd': ')',
-        'e': '3',
-        'f': '=',
-        'i': '!',
-        'k': '<',
-        's': '$',
-        't': '+',
-        'z': '2'
-    };
-  
-    const letters = Object.keys(replaces).filter(letter => Array.from(words.replace('[A-Z]','')).indexOf(letter) !== -1);
-    const letter = letters[Math.floor(Math.random() * letters.length)];
+function capitalize(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+function gen(type,adjective=[],noun=[]){
+    type=type.replace('{','').replace('}','');
+    resp="";
+    if(type){
+        if(type == "цифра"){
+            resp=Math.floor(Math.random() * 10);
+        }else{
+            const replaces = {
+                'a': '@',
+                'c': '(',
+                'd': ')',
+                'e': '3',
+                'f': '=',
+                'i': '!',
+                'k': '<',
+                's': '$',
+                't': '+',
+                'z': '2'
+            };
+            args="";
+            c=false;
+            if(type.includes("Іменник") || type.includes("Прикметник")){
+                c=true;
+            }
+            if(type.includes("прикметник") || type.includes("Прикметник")){
+                list=adjective;
+                args=type.replace("прикметник","");
+            }
+            if(type.includes("іменник") || type.includes("Іменник")){
+                list=noun;
+                args=type.replace("іменник","");
+            }
+            resp=list[Math.floor(Math.random() * list.length)].trim().toLowerCase();
 
-    if (type == "simple"){
-        text = words.toLowerCase();
-    }
-    if (type == "middle"){
-        text = words.replace(letter, replaces[letter]);
-    }
-    if (type == "strong"){
-        text = words
-        for (const [key, value] of Object.entries(replaces)) {
-            text = text.replaceAll(key, value);
+            if(c){resp=capitalize(resp);}
+
+            if(args.includes("-середній")){
+                const letters = Object.keys(replaces).filter(letter => Array.from(resp.replace('[A-Z]','')).indexOf(letter) !== -1);
+                const letter = letters[Math.floor(Math.random() * letters.length)];
+                resp = resp.replace(letter, replaces[letter]);
+            }
+            if(args.includes("-складний")){
+                const letters = Object.keys(replaces).filter(letter => Array.from(resp.replace('[A-Z]','')).indexOf(letter) !== -1);
+                const letter = letters[Math.floor(Math.random() * letters.length)];
+                for (const [key, value] of Object.entries(replaces)) {
+                    resp = resp.replaceAll(key, value);
+                }
+            }
         }
     }
-    return `${text}${numbers}`;
+    return resp;
+}
+
+function genPassword(adjective=[], noun=[], mask="{прикметник}{іменник}{цифра}{цифра}"){
+    resp="";
+
+    mask.split('}{').forEach((element) => {
+        resp+=gen(element,adjective,noun);
+    });
+
+    return resp;
 }
   
-document.querySelectorAll('.btn').forEach(button => {
+document.querySelectorAll('a.mask').forEach(button => {
     button.addEventListener('click', function() {
-        list1 = document.querySelector('.list1').textContent.trim().split('\n');
-        list2 = document.querySelector('.list2').textContent.trim().split('\n');
-        document.querySelector('.password').value=genPassword(list1, list2, button.id);
+        document.querySelector('input.mask').value=button.getAttribute('mask');
     });
+});
+
+document.querySelector('.generate').addEventListener('click', function() {
+    list1 = document.querySelector('.list1').textContent.trim().split('\n');
+    list2 = document.querySelector('.list2').textContent.trim().split('\n');
+    document.querySelector('.password').value=genPassword(list1, list2,document.querySelector('input.mask').value);
 });
